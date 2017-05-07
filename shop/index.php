29ec80cd9046067cad9a3749b41e77500be5381f
lbmzorx <lbmzorx@163.com>
2017-05-07 21:47:18 +0800

    New shop system created by ThinkPHP3.2.3,which compare to Yii frame and just for exercies!

---
+++ shop/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/7
+ * Time: 21:38
+ */
+
+define('APP_PATH','./Application/');
+define('APP_DEBUG',True);           //调试模式 -开
+//define('APP_DEBUG',false);           //调试模式 -关
+require '../ThinkPHP/ThinkPHP.php';
