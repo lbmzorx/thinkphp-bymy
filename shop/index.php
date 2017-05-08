e73bc7f44b9d8d4ed95cf87a999354321652c84a
lbmzorx <lbmzorx@163.com>
2017-05-08 23:00:35 +0800

    -------初始工作--------------

--- shop/index.php
+++ shop/index.php
@@ -6,7 +6,14 @@
  * Time: 21:38
  */
 
-define('APP_PATH','./Application/');
+//define('APP_PATH','./Application/');
 define('APP_DEBUG',True);           //调试模式 -开
 //define('APP_DEBUG',false);           //调试模式 -关
+/*
+ * 在自动生成目录结构的同时，在各个目录下面我们还看到了index.html文件，这是ThinkPHP自动生成的目录安全文件。
+ * 为了避免某些服务器开启了目录浏览权限后可以直接在浏览器输入URL地址查看目录，系统默认开启了目录安全文件机制，会在自动生成目录的时候生成空白的index.html文件
+ */
+//define('BUILD_DIR_SECURE', false); //环境足够安全，不希望生成目录安全文件，可以在入口文件里面关闭目录安全文件的生成，关闭自动生成文件
+
+
 require '../ThinkPHP/ThinkPHP.php';
